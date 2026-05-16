35c8b24c3a0660d32fce0ca0b9d3088f0f7bd25e
chante9172-cmd <chante9172@smail.hc.edu.tw>
2026-05-17 01:30:20 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='mt-v7';const A=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(!e.request.url.startsWith('http'))return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
